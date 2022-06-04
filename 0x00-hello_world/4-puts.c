592782d793666edd50b967993e69f277b532fea7
root <emma.smart@gg>
2022-06-03 19:46:59 -0700

    amother commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,30 +1,15 @@
-#!/bin/bash
-#include <stdio.h>
+   #include <stdio.h>
 
+   /**
+    * main - Entry point
+    *
+    * Description: A C program that prints with put function
+    *
+    * Return: Always 0 (Success)
+   */
 
-
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Description: A C program that prints with put function
- *
- *     *
- *
- *      * Return: Always 0 (Success)
- *
- *      */
-
-
-
-int main(void)
-
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
-}
+   int main(void)
+   {
+	   puts("\"Programming is like building multilingual puzzle");
+	   return (0);
+   }
