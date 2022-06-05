38719d425c11350d04517a01a20e5633cd16d403
root <emma.smart@gg>
2022-06-05 04:39:19 -0700

    another commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+*  main - Entry point
+* Description: A C program that prints with printf function.
+* this program prints "Programming is like building a multilingual puzzle"
+* Return: 0
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
