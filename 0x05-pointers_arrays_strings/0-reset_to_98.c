c55875afa753301b642484ff87e43ce8d2ae9d3e
root <emma.smart@gg>
2022-06-13 02:38:17 -0700

    another commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * 0-reset_to_98 - updates the value of n to 98
+ * @n: declaration of n and parameters for the function reset_to_98
+ *
+ * Return: Nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
