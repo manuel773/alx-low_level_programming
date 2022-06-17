364932c70ddc994b09c63844fada7bb9a57876f8
root <emma.smart@gg>
2022-06-17 13:55:48 -0700

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -16,6 +16,7 @@ char *_strcat(char *dest, char *src)
 	/*find the size of dest array*/
 	while (dest[c])
 		c++;
+
 	/* iterate through each src array value without the null byte*/
 	for (c2 = 0; src[c2] ; c2++)
 	/*append src[c2] to dest[c] while overwritting the null byte in dest*/
