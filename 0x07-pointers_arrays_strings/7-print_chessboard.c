f67a5312fe5e7635c84dd60b9aab07c61b7ee01a
root <emma.smart@gg>
2022-06-21 21:00:27 -0700

    another commit

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,14 +1,15 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_chessboard - prints chessboard.
- * @a: matrix
+ * print_chessboard - prints the chess board
+ * @a: input pointer
+ * Return: return nothing
  */
 
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
-	
+
 	i = 0;
 	j = 0;
 	while (i < 64)
