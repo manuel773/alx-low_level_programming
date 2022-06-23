ff5db4d6516ccffc069f76919fedf8095e1011f1
root <emma.smart@gg>
2022-06-23 13:48:06 -0700

    hot fix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,30 +1,28 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - main funct
- * @n: int n
- * Return: int
+ * _sqrt_recursion -> sqrt using recursion
+ * @n: parameter to be sqrt
+ * Return: square root of a number
  */
-
 int _sqrt_recursion(int n)
 {
 	return (_sqrt(n, 1));
 }
 
 /**
- * _sqrt - _sqrt_recursion
- * @n: integer paramter
- * @i: integer parameter
- * Return: sqrt
+ * _sqrt -> recursive square root
+ * @n: number
+ * @i: iterator
+ * Return: a number
  */
-
 int _sqrt(int n, int i)
 {
-	if (n < 0)
+	int square = i * i;
+
+	if (square > n)
 		return (-1);
-	if ((i * i) > n)
-		return (-1);
-	if (i * i == n)
+	if (square == n)
 		return (i);
 	return (_sqrt(n, i + 1));
 }
