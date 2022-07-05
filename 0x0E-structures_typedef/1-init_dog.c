df2faf3508f983d335c3954e1cb0d9ec26570055
root <emma.smart@gg>
2022-07-05 12:15:29 -0700

    another commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,14 +1,13 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
- * init_dog - Initializes a variable of type struct dog
- * @d: The dog to be initialized
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
+ * init_dog - initializes a variable of type struct dog
+ * @d: struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: no return
  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
