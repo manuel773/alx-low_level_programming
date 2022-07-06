ad9893cb83c8861e47355c326fe879a77abaee7d
root <emma.smart@gg>
2022-07-06 14:34:15 -0700

    another commit

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,15 @@
+#include "3-calc.h"
+
+/**
+ * op_add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtraction operator
