483f24ed9b368e0f06a0c8057e73403b3f51bb63
root <emma.smart@gg>
2022-07-07 15:45:39 -0700

    another commit

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,10 @@
 #include "3-calc.h"
 
 /**
- * op_add - addition operator
- * @a: first int
- * @b: second int
- * Return: sum of 2 numbers
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: add.
  */
 int op_add(int a, int b)
 {
@@ -12,4 +12,55 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - subtraction operator
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: difference.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: multiplication.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: division.
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
+ * Return: remainder of the division.
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
